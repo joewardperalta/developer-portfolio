1b202b61bbf7070456ee3d2b4eee66509d56a6b1
Joe Peralta <jperalta11@myseneca.ca>
2024-11-07 17:38:04 -0500

    Created the hero section

--- app/page.js
+++ app/page.js
@@ -1,6 +1,10 @@
+import PrimaryButton from "@/components/buttons/PrimaryButton";
 import Column from "@/components/Column";
 import Container from "@/components/Container";
 import Navbar from "@/components/Navbar";
+import SubTitle from "@/components/SubTitle";
+import Title from "@/components/Title";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -8,7 +12,28 @@ export default function Home() {
       <header>
         <Navbar />
       </header>
-      <main></main>
+
+      <main>
+        <section className="py-20" id="hero">
+          <Container>
+            <Column className="mb-10">
+              <Title>Full-Stack Web Developer</Title>
+              <SubTitle>— Based in Toronto, Canada</SubTitle>
+              <PrimaryButton>Get my resume</PrimaryButton>
+            </Column>
+
+            <Column className="h-[380px]">
+              <Image
+                className="rounded-default h-full w-full object-cover"
+                src="/Joeward Peralta Portrait.jpeg"
+                width={2000}
+                height={2000}
+                alt="Joeward peralta portrait wearing a formal beige turtle neck"
+              />
+            </Column>
+          </Container>
+        </section>
+      </main>
     </>
   );
 }
