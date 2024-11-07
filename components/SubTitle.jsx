08f5dbc282a7060237656e6034241548ce47e09d
Joe Peralta <jperalta11@myseneca.ca>
2024-11-07 17:36:09 -0500

    Created the title and subtitle components

---
+++ components/SubTitle.jsx
@@ -0,0 +1,3 @@
+export default function SubTitle({ children }) {
+  return <p className="mb-6 text-lg leading-normal">{children}</p>;
+}
---
+++ components/Title.jsx
@@ -0,0 +1,7 @@
+export default function Title({ children }) {
+  return (
+    <h1 className="text-secondary mb-4 text-5xl font-semibold uppercase leading-tight">
+      {children}
+    </h1>
+  );
+}
