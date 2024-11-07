0c0ca5411728a082583178da95aa92581a3530b0
Joe Peralta <jperalta11@myseneca.ca>
2024-11-07 18:59:41 -0500

    Created the heading component

---
+++ components/Heading.jsx
@@ -0,0 +1,7 @@
+export default function Heading({ children }) {
+  return (
+    <h2 className="text-secondary mb-2.5 text-center text-2xl font-semibold uppercase leading-normal">
+      {children}
+    </h2>
+  );
+}
