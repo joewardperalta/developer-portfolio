73d68f23771a6548d36210c364b9997df6704945
Joe Peralta <jperalta11@myseneca.ca>
2024-11-07 20:56:26 -0500

    Added role to the experience card

--- components/ExperienceCard.jsx
+++ components/ExperienceCard.jsx
@@ -1,13 +1,20 @@
-export default function ExperienceCard({ title, startDate, endDate, bullets }) {
+export default function ExperienceCard({
+  title,
+  startDate,
+  endDate,
+  role,
+  bullets,
+}) {
   return (
     <div>
-      <div>
+      <div className="mb-2">
         <h3 className="text-secondary text-lg font-semibold leading-normal">
           {title}
         </h3>
-        <p className="text-tertiary mb-2 font-semibold leading-normal">
+        <p className="text-tertiary font-semibold leading-normal">
           {startDate.month} {startDate.year} - {endDate.month} {endDate.year}
         </p>
+        <p className="text-quaternary font-semibold leading-normal">{role}</p>
       </div>
       <ul className="list-disc px-5">
         {bullets.map((bullet, index) => (
