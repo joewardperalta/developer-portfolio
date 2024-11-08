57b2c378d9df5fd1c185ef9dd23e9cba287b14be
Joe Peralta <jperalta11@myseneca.ca>
2024-11-08 09:38:42 -0500

    Change the overall aesthetic of the button by changing the font-weight to semibold so that it pops more than the border. I also changed the button border color to a much lighter black. In addition, it is also has been optimized for tablet device by changing the text size.

--- components/buttons/Button.jsx
+++ components/buttons/Button.jsx
@@ -2,7 +2,7 @@ export default function Button({ children, className, onClick, ref }) {
   return (
     <button
       ref={ref}
-      className={`${className} rounded-full px-5 py-2.5 text-sm`}
+      className={`${className} tablet:text-base tablet:px-7 tablet:py-4 rounded-full px-5 py-2.5 text-sm font-semibold`}
       onClick={onClick}
     >
       {children}
--- components/buttons/PrimaryButton.jsx
+++ components/buttons/PrimaryButton.jsx
@@ -4,7 +4,7 @@ export default function PrimaryButton({ children, onClick, className, ref }) {
   return (
     <Button
       ref={ref}
-      className={`${className} border-secondary border`}
+      className={`${className} border border-tertiary`}
       onClick={onClick}
     >
       {children}
