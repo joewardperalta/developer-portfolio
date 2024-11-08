c102205310b249ad5040c136e03d08df02cfc149
Joe Peralta <jperalta11@myseneca.ca>
2024-11-07 19:01:35 -0500

    Created the technologies section

--- app/page.js
+++ app/page.js
@@ -6,6 +6,11 @@ import SubTitle from "@/components/SubTitle";
 import Title from "@/components/Title";
 import Image from "next/image";
 import Section from "@/components/Section";
+import Heading from "@/components/Heading";
+import TechCard from "@/components/TechCard";
+
+// Data
+import technologies from "@/data/technologies.json";
 
 export default function Home() {
   return (
@@ -33,6 +38,26 @@ export default function Home() {
                 alt="Joeward peralta portrait wearing a formal beige turtle neck"
                 priority
               />
+            </Column>
+          </Container>
+        </Section>
+
+        {/* Technologies */}
+        <Section id="technologies">
+          <Container>
+            <Column>
+              <Heading>Technologies</Heading>
+            </Column>
+          </Container>
+          <Container>
+            <Column>
+              <ul className="grid grid-cols-3 justify-center gap-6">
+                {technologies.map((tech, index) => (
+                  <li key={index}>
+                    <TechCard title={tech.title} img={tech.img} />
+                  </li>
+                ))}
+              </ul>
             </Column>
           </Container>
         </Section>
