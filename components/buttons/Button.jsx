1086ffe97c1bac9934f689010c43b247ccad2af5
Joe Peralta <jperalta11@myseneca.ca>
2024-11-08 09:47:27 -0500

    Change the line-height

--- components/buttons/Button.jsx
+++ components/buttons/Button.jsx
@@ -2,7 +2,7 @@ export default function Button({ children, className, onClick, ref }) {
   return (
     <button
       ref={ref}
-      className={`${className} tablet:text-base tablet:px-7 tablet:py-4 rounded-full px-5 py-2.5 text-sm font-semibold`}
+      className={`${className} tablet:text-base tablet:px-7 tablet:py-4 tablet:leading-[1.2] rounded-full px-5 py-2.5 text-sm font-semibold leading-[1.2]`}
       onClick={onClick}
     >
       {children}
