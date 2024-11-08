44d028a4385c8007a5954a8280feaea62ea33fad
Joe Peralta <jperalta11@myseneca.ca>
2024-11-08 10:13:09 -0500

    Updated the font-size and the margin bottom to align the design for tablet size.

--- components/Typography/Heading.jsx
+++ components/Typography/Heading.jsx
@@ -1,6 +1,6 @@
 export default function Heading({ children }) {
   return (
-    <h2 className="text-secondary mb-2.5 text-center text-2xl font-semibold uppercase leading-normal">
+    <h2 className="tablet:text-[2rem] tablet:mb-12 mb-2.5 text-center text-2xl font-semibold uppercase leading-normal text-secondary">
       {children}
     </h2>
   );
