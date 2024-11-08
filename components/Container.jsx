fd7c66203692faeda3da83afb462a96acb508239
Joe Peralta <jperalta11@myseneca.ca>
2024-11-08 09:04:05 -0500

    Modified the horizontal space designed for tablet device

--- components/Container.jsx
+++ components/Container.jsx
@@ -1,3 +1,3 @@
 export default function Container({ children }) {
-  return <div className="flex flex-wrap px-5">{children}</div>;
+  return <div className="tablet:px-8 flex flex-wrap px-5">{children}</div>;
 }
