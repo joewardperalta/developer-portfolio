6a84a9c237832d0b3e673aefbe5b9e5671d62397
Joe Peralta <jperalta11@myseneca.ca>
2024-11-09 13:05:54 -0500

    Revert back to normal

--- components/Typography/Title.jsx
+++ components/Typography/Title.jsx
@@ -1,6 +1,6 @@
 export default function Title({ children }) {
   return (
-    <h1 className="tablet:text-[5.5rem] mb-4 text-5xl font-semibold uppercase leading-[1.2] text-secondary">
+    <h1 className="mb-4 text-5xl font-semibold uppercase leading-[1.2] text-secondary tablet:text-[5.5rem]">
       {children}
     </h1>
   );
