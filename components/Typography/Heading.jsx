efe4e5c569c1abb37bd746eaa47dbdebfa72ef9f
Joe Peralta <jperalta11@myseneca.ca>
2024-11-09 09:57:09 -0500

    Format document

--- components/Typography/Heading.jsx
+++ components/Typography/Heading.jsx
@@ -1,6 +1,6 @@
 export default function Heading({ children }) {
   return (
-    <h2 className="tablet:text-[2rem] tablet:mb-12 mb-2.5 text-center text-2xl font-semibold uppercase leading-normal text-secondary">
+    <h2 className="mb-2.5 text-center text-2xl font-semibold uppercase leading-normal text-secondary tablet:mb-12 tablet:text-[2rem]">
       {children}
     </h2>
   );
