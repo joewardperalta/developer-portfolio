4e1473377b0bb109143f4cc5a77dc6ca7b62905a
Joe Peralta <jperalta11@myseneca.ca>
2024-11-09 12:33:57 -0500

    Fix column component not propery adjusting for tablet and desktop device

--- components/Column.jsx
+++ components/Column.jsx
@@ -1,7 +1,7 @@
 export default function Column({ children, className }) {
   return (
     <div
-      className={`${className} shrink grow basis-full tablet:basis-1/3`}
+      className={`${className} shrink grow basis-full tablet:basis-1/2 desktop:basis-1/12`}
     >
       {children}
     </div>
