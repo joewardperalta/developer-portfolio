64471fc162a0540f25e874c4b4e2b51b77bb356a
Joe Peralta <jperalta11@myseneca.ca>
2024-11-09 09:54:22 -0500

    Change default button text color

--- components/buttons/Button.jsx
+++ components/buttons/Button.jsx
@@ -1,8 +1,13 @@
-export default function Button({ children, className, onClick, ref }) {
+export default function Button({
+  children,
+  className,
+  onClick,
+  ref,
+}) {
   return (
     <button
       ref={ref}
-      className={`${className} tablet:text-base tablet:px-7 tablet:py-4 tablet:leading-[1.2] rounded-full px-5 py-2.5 text-sm font-semibold leading-[1.2]`}
+      className={`${className} rounded-full px-5 py-2.5 text-sm font-semibold leading-[1.2] text-secondary tablet:px-7 tablet:py-4 tablet:text-base tablet:leading-[1.2]`}
       onClick={onClick}
     >
       {children}
