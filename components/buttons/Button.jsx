670a2bae7d29101b25b859f6613840ac51e0ded6
Joe Peralta <jperalta11@myseneca.ca>
2024-11-11 07:53:07 -0500

    Change the transition effect to have ease in out

--- components/buttons/Button.jsx
+++ components/buttons/Button.jsx
@@ -7,7 +7,7 @@ export default function Button({
   return (
     <button
       ref={ref}
-      className={`${className} rounded-full px-5 py-2.5 text-sm font-semibold leading-[1.2] text-secondary tablet:px-7 tablet:py-4 tablet:text-base tablet:leading-[1.2]`}
+      className={`${className} rounded-full px-5 py-2.5 text-sm font-semibold leading-[1.2] text-secondary transition duration-300 ease-in-out tablet:px-7 tablet:py-4 tablet:text-base tablet:leading-[1.2]`}
       onClick={onClick}
     >
       {children}
--- components/buttons/PrimaryButton.jsx
+++ components/buttons/PrimaryButton.jsx
@@ -9,7 +9,7 @@ export default function PrimaryButton({
   return (
     <Button
       ref={ref}
-      className={`${className} border border-tertiary transition-all hover:bg-secondary hover:text-primary`}
+      className={`${className} border border-tertiary hover:bg-secondary hover:text-primary`}
       onClick={onClick}
     >
       {children}
