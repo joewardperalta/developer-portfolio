37c38f64bdfa8c20595ec964ff492f79864f562e
Joe Peralta <jperalta11@myseneca.ca>
2024-11-16 23:42:19 -0500

    Re-design the navigation bar for desktop devie

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,116 +1,48 @@
-"use client";
 import Link from "next/link";
-import Image from "next/image";
-import { useRef } from "react";
-
-// Data
-import socials from "@/data/socials.json";
 
 export default function Navbar() {
-  const navRef = useRef(null);
-
-  function showNav() {
-    // check to make sure navRef is not null, if not null,
-    // then we successfully got the nav element as refence
-    if (!navRef.current) {
-      return 0;
-    }
-
-    // show the nav
-    navRef.current.classList.toggle("hidden");
-
-    // disable browser scrolling
-    document.body.classList.toggle("overflow-hidden");
-    document.body.classList.toggle("h-full");
-  }
-
   return (
-    <div className="px-5 py-6 tablet:px-8 tablet:py-10 desktop:mx-auto desktop:max-w-screen-desktop desktop:px-24">
-      <div className="relative z-50 flex w-full justify-between">
-        <Logo />
-        <div className="flex gap-6">
-          <ul className="flex items-center gap-3">
-            {socials.map((social, index) => (
-              <li key={index} className="h-[24px] w-[24px]">
-                <Link href={social.link} target="_blank">
-                  <Image
-                    className="h-full w-full"
-                    src={social.img.src}
-                    alt={social.img.alt}
-                    width={500}
-                    height={500}
-                  />
-                </Link>
-              </li>
-            ))}
-          </ul>
-
-          <HamburgerButton onClick={showNav} />
-        </div>
-      </div>
-      <Nav ref={navRef} handleNav={showNav} />
+    <div className="mx-auto flex justify-between py-10 desktop:max-w-[1248px]">
+      <Logo />
+      <Nav />
     </div>
   );
 }
 
 function Logo() {
   return (
-    <div>
-      <a
-        className="font-bold uppercase text-secondary tablet:text-lg"
-        href="#"
-      >
-        Joeward Peralta
-      </a>
-    </div>
+    <Link
+      className="text-[1.125rem] font-bold uppercase leading-[1.6] text-secondary"
+      href="#"
+    >
+      Joeward Peralta
+    </Link>
   );
 }
 
-function HamburgerButton({ onClick }) {
+function Nav() {
   return (
-    <button
-      className="space-y-1"
-      onClick={onClick}
-      aria-label="Hamburger button"
-    >
-      <div className="h-1 w-8 rounded-full bg-secondary"></div>
-      <div className="h-1 w-8 rounded-full bg-secondary"></div>
-    </button>
-  );
-}
-
-function Nav({ ref, handleNav }) {
-  return (
-    <div
-      className="absolute left-0 top-0 z-10 hidden h-screen w-full bg-primary px-5 pb-6 pt-32 tablet:px-8"
-      ref={ref}
-    >
-      <div className="desktop:mx-auto desktop:max-w-screen-desktop desktop:px-24">
-        <nav>
-          <ul className="space-y-7 font-bold uppercase text-secondary">
-            <li>
-              <Link href="#technologies" onClick={handleNav}>
-                Technologies
-              </Link>
-            </li>
-            <li>
-              <Link href="#experience" onClick={handleNav}>
-                Experience
-              </Link>
-            </li>
-            <li>
-              <Link href="#projects" onClick={handleNav}>
-                Projects
-              </Link>
-            </li>
-            <li>
-              <Link href="#contacts" onClick={handleNav}>
-                Contacts
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
+    <nav>
+      <ul className="flex gap-8 text-[1rem] font-medium leading-normal text-secondary">
+        <li>
+          <Link href="#">Home</Link>
+        </li>
+        <li>
+          <Link href="#about">About</Link>
+        </li>
+        <li>
+          <Link href="#experience">Experience</Link>
+        </li>
+        <li>
+          <Link href="#portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="#testimonials">Testimonials</Link>
+        </li>
+        <li>
+          <Link href="#contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
