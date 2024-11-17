3a3eb47b25577e72cd6f4c983c21aa401e3c5ec9
Joe Peralta <jperalta11@myseneca.ca>
2024-11-16 23:04:20 -0500

    Create a component for all the socials

---
+++ components/SocialList.jsx
@@ -0,0 +1,26 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+
+// data
+import socials from "@/data/socials.json";
+import { useEffect } from "react";
+
+export default function SocialList({ className, light = true }) {
+  return (
+    <ul className={`${className} flex w-fit gap-6`}>
+      {socials.map((social, index) => (
+        <li key={index}>
+          <Link href={social.link}>
+            <Image
+              src={light ? social.img.light.src : social.img.dark.src}
+              alt={social.img.alt}
+              width={32}
+              height={32}
+            />
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
