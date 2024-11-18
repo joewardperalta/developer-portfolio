60e39265c56ef411c1f5a1da1f3f1f7ad323e81d
Joe Peralta <jperalta11@myseneca.ca>
2024-11-18 00:05:11 -0500

    Make the section heading a lot bigger

--- components/Typography/Heading.jsx
+++ components/Typography/Heading.jsx
@@ -1,7 +1,7 @@
 export default function Heading({ children, className }) {
   return (
     <h2
-      className={`${className} font-medium uppercase leading-[1.2] desktop:mb-[9.375rem] desktop:text-[6.125rem]`}
+      className={`${className} w-full text-center font-medium uppercase leading-[1.2] text-secondary desktop:mb-[9.375rem] desktop:text-[6.125rem]`}
     >
       {children}
     </h2>
