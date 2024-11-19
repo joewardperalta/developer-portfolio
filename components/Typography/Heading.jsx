26afe2f47ae56f3af75e8f21a407a68cf0e60bd3
Joe Peralta <jperalta11@myseneca.ca>
2024-11-19 16:43:06 -0500

    Change font size for the Heading component for each screen size

--- components/Typography/Heading.jsx
+++ components/Typography/Heading.jsx
@@ -1,7 +1,7 @@
 export default function Heading({ children, className }) {
   return (
     <h2
-      className={`${className} w-full text-center font-medium uppercase leading-[1.2] text-secondary desktop:mb-[9.375rem] desktop:text-[6.125rem]`}
+      className={`${className} mb-[4.5rem] w-full text-center text-[2.625rem] font-medium uppercase leading-[1.2] text-secondary tablet:mb-[6rem] tablet:text-[4.125rem] laptop:mb-[9.375rem] laptop:text-[5.125rem] desktop:mb-[9.375rem] desktop:text-[6.125rem]`}
     >
       {children}
     </h2>
