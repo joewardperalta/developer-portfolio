d26532cdd2060d7900aa63c16d0f0cbee1a3a0cc
Joe Peralta <jperalta11@myseneca.ca>
2024-11-19 13:21:26 -0500

    Change the font size to 56px when viewing in mobile device, 88px in tablet device, 112px in laptop, and 136px in desktop

--- components/Typography/Title.jsx
+++ components/Typography/Title.jsx
@@ -1,6 +1,6 @@
 export default function Title({ children }) {
   return (
-    <h1 className="mb-4 font-medium uppercase leading-[1.2] text-secondary desktop:text-[8.5rem]">
+    <h1 className="mb-4 text-[3.5rem] font-medium uppercase leading-[1.2] text-secondary tablet:text-[5.5rem] laptop:text-[7rem] desktop:text-[8.5rem]">
       {children}
     </h1>
   );
