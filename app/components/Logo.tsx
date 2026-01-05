39f0b706f1b405cd78fb67397453d435a2faa516
Joeward Peralta <joeward_peralta@outlook.com>
2026-01-05 17:51:54 -0500

    Remove the components folder inside the app folder because it's not being used

--- app/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Logo() {
-  return (
-    <Link href="/" className="text-2xl font-bold text-heading">
-      joewardperalta.dev
-    </Link>
-  );
-}
--- app/components/Wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-interface Props {
-  children?: ReactNode;
-}
-
-export default function Wrapper({ children }: Props) {
-  return <div className="max-w-200 px-6 mx-auto">{children}</div>;
-}
